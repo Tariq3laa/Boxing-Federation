9553e471c2194444f855398e98b09aa8480defc9
Tarek Alaa <ta11013997@gmail.com>
2021-01-12 17:00:30 +0200

    Edit 5

--- resources/views/mail/coach/verification.blade.php
+++ resources/views/mail/coach/verification.blade.php
@@ -4,7 +4,7 @@ Welcome to our Website!
 <br>
 Please click the below link to verify your email and activate your account!
 <br><br>
-<a href="{{\URL::to('/')}}/coachs/verify?code={{$email_data['verification_code']}}">Click Here!</a>
+<a href="{{\URL::to('/')}}/api/coachs/verify?code={{$email_data['verification_code']}}">Click Here!</a>
 <br><br>
 Thank you!
 <br>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-Route::get('coachs/verify', 'CoachController@verify');
-Route::get('tecs/verify', 'TecController@verify');
